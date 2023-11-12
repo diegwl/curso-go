a0c00e08ecaf646309ee3f08b94f215b6784070f
diegwl <diegocastanlopes0210@outlook.com>
2023-11-12 17:09:42 -0300

    funções

---
+++ aula04 - Funções/funcoes.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
+func main() {
+	soma := somar(2, 3)
+	fmt.Println(soma)
+
+	var f = func(txt string) string {
+		fmt.Println(txt)
+		return txt
+	}
+
+	resultado := f("Texto da função 1")
+	fmt.Printf("Retorno da função 1: %s", resultado)
+
+	resultadoSoma, resultadoSubtracao := calculosMatematicos(10, 15)
+	fmt.Println(resultadoSoma, resultadoSubtracao)
+
+	resultadoSoma2, _ := calculosMatematicos(10, 5)
+	fmt.Println(resultadoSoma2)
+}
